a421f5b6a24cdabe08014c6f5501580404549ab8
meltrust <original.mtapia@outlook.com>
2021-08-13 05:45:18 -0500

    Add localStorage functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,30 @@
-// empty for init
+// localStorage
+
+function allBooks() {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+  return books;
+}
+
+function addBooks(newBook) {
+  const getAllBooks = allBooks();
+  getAllBooks.push(newBook);
+  localStorage.setItem('books', JSON.stringify(getAllBooks));
+}
+
+function deleteBooks(index) {
+  const getAllBooks = allBooks();
+
+  getAllBooks.splice(index, 1);
+
+  localStorage.setItem('books', JSON.stringify(getAllBooks));
+}
+
+// addBooks({ title: 'title2', author: 'author2' });
+// deleteBooks(2);
+// console.log(allBooks());
+// localStorage.clear();
